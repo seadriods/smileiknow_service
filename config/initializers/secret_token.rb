d1ff6e15367e37ca3fe0d70e6e366c9302226322
Rao Lakkakula <nlakkakula@gmail.com>
2011-06-25 15:58:35 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smileiknow::Application.config.secret_token = 'c1f4434ccb007de8933e61401c4eadba995642f5ca7438fb49cba30b8bf621ef8feebc05ffad000dc9986fb8b49790bb8ea7c899146e483b62811cbe76b4f8f5'
